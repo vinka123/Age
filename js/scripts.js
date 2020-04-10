7dabc5ba0b9a9a284560b615f378e745e4c2d3db
vinka123 <ktnvctr123@gmail.com>
2020-04-10 12:11:19 +0300

    Add js code in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,17 @@ alert('You will be going to the club but no DRINKS for you');
 } else{
 alert('Underage!!Stay with your mom ) :');
 }
+//Another program i created for determining your grade
+alert('This is a simple app i created to determine your grade through your marks');
+var marks=parseInt(prompt('Enter your marks from 0-100'));
+if(marks<=20) {
+alert('Grade E');
+} else if(marks>=21 && marks<=39) {
+alert('Grade D');
+} else if(marks>=40 && marks<=59) {
+alert('Grade C');
+} else if(marks>=60 && marks<=79) {
+alert('Grade B');
+} else {
+alert('Grade A');
+}
